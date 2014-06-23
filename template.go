d3e851324e5864843e72d01aec6d5989cb2a31e7
Martijn van Maurik <martijn@vmaurik.nl>
2014-06-23 10:41:18 +0200

    Update code

--- template.go
+++ template.go
@@ -44,8 +44,8 @@ func contains(item map[string]string, key string) bool {
 	return false
 }
 
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
+func exists(file string) (bool, error) {
+	_, err := os.Stat(file)
 	if err == nil { return true, nil }
 	if os.IsNotExist(err) { return false, nil }
 	return false, err
